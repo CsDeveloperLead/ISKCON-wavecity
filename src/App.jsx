c5e794340ad0e8de9e03696ab05ccc85ecf3ba99
Aftab1311 <md.aftabehsan@gmail.com>
2024-10-08 20:06:26 +0530

    coming soon post made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="bg-gray-900 py-20">
+    <div className="max-w-4xl mx-auto text-center px-4">
+        <h2 className="text-4xl font-bold text-white mb-6">Coming Soon!</h2>
+        <p className="text-lg text-gray-300 mb-8">We're working on something amazing for you. Stay tuned!</p>
+        <div className="flex justify-center">
+            <div className="flex-shrink-0">
+                <a href="#" className="bg-white hover:bg-gray-100 text-gray-900 font-bold py-3 px-6 rounded-full inline-block transition duration-300">Subscribe</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </>
   )
 }
